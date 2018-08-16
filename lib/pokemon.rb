adcd31a997a3f368f2a45c8822a521069aa1ba92
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-16 20:59:11 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -17,12 +17,10 @@ class Pokemon
   
   def self.find(id, db)
     row = db.execute("SELECT * FROM pokemon WHERE id = ?", id);
-    # attributes = {
-    #   id: row[0][0],
-    #   name: row[0][1],
-    #   type: row[0][2],
-    #   db: db
-    # }
     self.new(id: row[0][0], name: row[0][1], type: row[0][2], db: db)
+  end
+  
+  def alter_hp(new_hp, db)
+    db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, self.id)
   end
 end
